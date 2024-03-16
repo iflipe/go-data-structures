9c774f66486024496b7e52fe57846d7c8f461b44
Filipe <hipolitofilipe@hotmail.com>
2024-03-16 13:50:25 -0300

    Remake main.go

--- main.go
+++ main.go
@@ -6,12 +6,4 @@ import (
 )
 
 func main() {
-	alpha := list.LinkedList{}
-	alpha.Add(8)
-	alpha.Add(2)
-	alpha.Add(8)
-	alpha.Add(1)
-	alpha.AddOnIndex(244, 3)
-	val, _ := alpha.Get(3)
-	fmt.Printf("%d is the first value in the Alpha list", val)
 }
